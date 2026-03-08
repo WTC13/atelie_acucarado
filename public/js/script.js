db548e05ee8a1b35914854b70b1d02fd4e0d6f18
Wendell Costa <wendell.timoteoc@gmail.com>
2026-03-08 01:22:26 -0300

    feat(ui) - create carousel

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,8 @@ tailwind.config = {
             colors: {
                 lavanda: '#D8B4F8',
                 roxoDoce: '#A668FF',
+                pascoaLavanda: '#E6D7FF',
+                pascoaRosa: '#FFD1DC',
                 chocolate: '#4B2E1E',
             }
         }
@@ -16,5 +18,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: { items: 1 },
+            768: { items: 2 },
+            1024: { items: 3 }
+        }
     });
 });
